3af9e8625c62742ca8303a00ce774abe2bac5625
aaronpeter20 <aaronpeter408@gmail.com>
2024-04-24 18:45:20 +0530

    attendance bug

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -2,9 +2,5 @@ module.exports = {
     HOST: "localhost",
     USER: "root",
     PASSWORD: null,
-<<<<<<< HEAD
     DB: "emp"
-=======
-    DB: "test" //Change with your db
->>>>>>> bf0d2598f6a0c1c2027c00c49deb8c0f81ff728d
   };
