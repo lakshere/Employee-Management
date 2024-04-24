f28a698766a7a7b17c8aaefa24255dc0b697952b
JK432 <jk04032002@gmail.com>
2024-04-24 07:36:04 +0530

    commit

--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -12,13 +12,12 @@ module.exports = app =>{
     // Retrieve a single user with id
     router.get("/:user_id", user.findOne);
 
-<<<<<<< HEAD
     // Update a User with id
     router.put("/:id", user.update);
-=======
+
     // Delete a user with id
     router.delete("/:user_id", user.delete);
->>>>>>> 063ca062c8caa34a82e0b3ac57d52b54e23be834
+
 
     app.use('/api/users', router);
 
