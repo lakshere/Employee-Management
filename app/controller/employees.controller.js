3a35c9cd98c4ad127e7cc22590ea021388853356
JK432 <jk04032002@gmail.com>
2024-04-24 07:45:04 +0530

    added employee update controler

--- app/controller/employees.controller.js
+++ app/controller/employees.controller.js
@@ -58,6 +58,38 @@ exports.findOne = (req, res) => {
   });
 };
 
+
+// Update a single employee with a id
+exports.update = (req, res) => {
+  // Validate Request
+  if (!req.body) {
+    res.status(400).send({
+      message: "Content can not be empty!"
+    });
+  }
+
+  console.log(req.body);
+
+  Employees.updateById(
+    req.params.id,
+    new Tutorial(req.body),
+    (err, data) => {
+      if (err) {
+        if (err.kind === "not_found") {
+          res.status(404).send({
+            message: `Not found Tutorial with id ${req.params.id}.`
+          });
+        } else {
+          res.status(500).send({
+            message: "Error updating Tutorial with id " + req.params.id
+          });
+        }
+      } else res.send(data);
+    }
+  );
+};
+
+
 // Delete a user with the specified id in the request
 exports.delete = (req, res) => {
   Employees.remove(req.params.employee_id, (err, data) => {
@@ -74,4 +106,7 @@ exports.delete = (req, res) => {
     } else res.send({ message: `Employee was deleted successfully!` });
   });
 };
+
+
+
 
